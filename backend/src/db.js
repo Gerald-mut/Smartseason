f6b8e4b617b742260fd4bc3ed2c1e0bfbee5e92a
Gerald Muteru <muterugerald@gmail.com>
2026-04-21 15:03:18 +0300

    Feat:Added the supabase client

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,6 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
 
+//create a supabase client instance
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
